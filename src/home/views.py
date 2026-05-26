ac78b27b2bc173de622c6091dfdb449e10d41c73
kaikysousa <kaikydsousa@gmail.com>
2026-05-26 16:27:17 -0300

    Adiciona os arquivos estátidos desenvolvidos no figma como templates do django

--- src/home/views.py
+++ src/home/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class DashboardView(TemplateView):
+    template_name = 'home/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_page'] = 'dashboard'
+        return context
+
+
+class ProposicoesView(TemplateView):
+    template_name = 'home/proposicoes.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_page'] = 'proposicoes'
+        return context
+
+
+class VotacoesView(TemplateView):
+    template_name = 'home/votacoes.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_page'] = 'votacoes'
+        return context
+
+
+class FavoritosView(TemplateView):
+    template_name = 'home/favoritos.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_page'] = 'favoritos'
+        return context
+
+
+class AlertasView(TemplateView):
+    template_name = 'home/alertas.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_page'] = 'alertas'
+        return context
+
+
+class UsuarioView(TemplateView):
+    template_name = 'home/usuario.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_page'] = 'usuario'
+        return context
