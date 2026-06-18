097982b3cada7c546e052c8128310e4169d63043
kaikysousa <kaikydsousa@gmail.com>
2026-06-18 10:26:10 -0300

    fix: Refatora para voltar a utilizar variável de ambiente para modo de debug

--- src/config/settings.py
+++ src/config/settings.py
@@ -50,8 +50,8 @@ def _database_port() -> int:
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY") or os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = bool(os.environ.get("DEBUG", default=0))
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
+
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 
