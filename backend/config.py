b4348e4729b5b2a0aeae82ce41b3d28ec4c1a45d
delvale412 <mvlameira@gmail.com>
2026-06-23 00:31:57 -0300

    build(backend): configuracao de dependencias e injecao de configuracoes

---
+++ backend/config.py
@@ -0,0 +1,16 @@
+# backend/config.py
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Prisma Insight API"
+    API_V1_STR: str = "/api/v1"
+    
+    # Variáveis mapeadas do .env
+    GITHUB_TOKEN: str
+    PROJECT_OWNER: str = "unb-mds"
+    PROJECT_REPO: str = "2026-1-P.R.I.S.M.A"
+
+    # Configuração para ler o arquivo .env
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    
+settings = Settings()
